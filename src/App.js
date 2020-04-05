13454f1d1cd6d6365d9a603c1f964c4dd2f1711c
Sri <srikaanth.00@gmail.com>
2020-04-05 14:29:51 +0530

    second part

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/menuComponent";
+import { DISHES } from "./resources/dishes";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Flechezo</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  state = {
+    dishes: DISHES,
+  };
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Flechezo</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
